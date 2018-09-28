52ddaa25d1e10482dc35cfca3dddecd696565345
HEINEMANN <idohaineman@gmail.com>
2018-09-28 15:03:16 +0300

    added cameras api for easy use of all cameras on the robot

--- tools/cameras.py
+++ tools/cameras.py
@@ -2,34 +2,59 @@ import cv2
 import numpy as np
 from threading import Lock
 
-class Camera:
+class Camera(cv2.VideoCapture):
     def __init__(self, index, const):
-        self.init = cv2.VideoCapture(index)
-        if not self.init.isOpened():
-            raise Exception('could not connect to camera')
-        self.constant = const
-        self.lock = Lock()
+        cv2.VideoCapture.__init__(self, index)
+        self.constant = float(const)
+        self.index = index
     def __del__(self):
-        with self.lock:
-            self.init.release()
-    def __call__(self, *args, **kwargs) -> np.ndarray:
-        with self.lock:
-            ok, frame = self.init.read()
-            return frame
+        self.release()
 
 
 class CameraList:
-    def __init__(self, cams, constants):
+    def __init__(self, cams, constants, select_cam=0):
         self.cameras = []
         self.camera = None
-        for i, c in enumerate(cams):
+        for i, c in enumerate(set(cams)):
             self.cameras.append(Camera(c, constants[i]))
         self.lock = Lock()
+        self.camera = self.cameras[select_cam]
+
     def __getitem__(self, item:int) -> cv2.VideoCapture:
         with self.lock:
             return self.cameras[item]
+
     def set_camera(self, index):
         with self.lock:
-            self.camera = index
+            self.camera = self.cameras[index]
+
     def __delitem__(self, key):
-        del self.cameras[key]
+        with self.lock:
+            if self.camera is self.cameras[key]:
+                self.camera = None
+            del self.cameras[key]
+
+    def read(self, *args) -> np.ndarray or list:
+        with self.lock:
+            if len(args) == 0:
+                return self.camera.read()
+            images = []
+            for i in set(args):
+                images.append(self.cameras[i].read())
+            return images
+
+    def add_camera(self, index:int or str, constant: float or int):
+        with self.lock:
+            self.cameras.append(Camera(index, constant))
+            self.sort()
+
+    def sort(self):
+        with self.lock:
+            self.cameras.sort(key=lambda x: x.index)
+
+    def set_camera_by_index(self, index:int or str):
+        with self.lock:
+            for i in self.cameras:
+                if i.index == index:
+                    self.camera = i
+                    return
--- tools/test_genetic_algorith.py
+++ tools/test_genetic_algorith.py
@@ -3,7 +3,7 @@ import numpy as np
 from tools.find_optimized_parameters import find_optimized_parameters
 import matplotlib.pyplot as plt
 
-def threashold(frame, params):
+def threshold(frame, params):
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     red, green, blue = params
     return cv2.inRange(frame, (red[0], green[0], blue[0]), (red[1], green[1], blue[1]))
@@ -25,19 +25,19 @@ def main():
         if k == ord('c'):
             cv2.destroyAllWindows()
             break
-    params, scores = find_optimized_parameters(threashold, src, boxes, (3,2), c_factor=5, alpha=10, survivors_size=50, gen_size=1000, gen_random=100, max_iter=100)
+    params, scores = find_optimized_parameters(threshold, src, boxes, (3, 2), c_factor=5, alpha=5, survivors_size=10, gen_size=1000, gen_random=100, max_iter=20)
     plt.plot(np.arange(len(scores)), scores)
     print(params)
     plt.show()
     while True:
         cv2.imshow('original', src[0])
-        cv2.imshow('threshold', threashold(src[0], params))
+        cv2.imshow('threshold', threshold(src[0], params))
         if cv2.waitKey() & 0xFF == ord('c'):
             cv2.destroyAllWindows()
             break
     while True:
         ok, frame = video.read()
-        cv2.imshow('threshold', threashold(frame, params))
+        cv2.imshow('threshold', threshold(frame, params))
         cv2.imshow('feed', frame)
         if cv2.waitKey(1) & 0xFF == ord('c'):
             cv2.destroyAllWindows()
