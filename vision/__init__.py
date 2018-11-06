bc6a1c8cf51731d9d2a477d5b4ece022e5f64892
karlo <chervoguy@gmail.com>
2018-11-06 20:02:17 +0200

    added stuff

--- vision/__init__.py
+++ vision/__init__.py
@@ -1,2 +1,2 @@
-from find_trash import find_trash as find_trash
-from find_fuels import find_fuels as find_fuels
+from find_trash import find_trash
+from find_fuels import find_fuels
--- vision/find_trash.py
+++ vision/find_trash.py
@@ -5,18 +5,21 @@ rtag = np.array([0, 0, 0, 0])
 
 
 def find_trash(frame, camera_data):
+    global rtag
+    rtag = np.array(rtag)
     thr = threshold_trash(frame)
     cnts = sorted_contours(thr)
     rects = contours_to_rects_sorted(cnts)
 
     if len(rects) > 0:
-        if rects[0][2]*rects[0][3] < globals()['rtag'][3]*globals()['rtag'][2]:
-            globals()['rtag'] = 0.8*globals()['rtag'] + 0.2*np.array(rects[0])
-            rects[0] = tuple(list(globals()['rtag'].astype(int)))
+        if rects[0][2]*rects[0][3] < rtag[3]*rtag[2]:
+            rtag = 0.99*rtag + 0.01*np.array(rects[0])
+            rects[0] = tuple(list(rtag.astype(int)))
         else:
-            globals()['rtag'] = rects[0]
+            rtag = rects[0]
     else:
-        rects.append(tuple(list(globals()['rtag'].astype(int))))
-    d = TRASH.distance_by_params(camera_data, 0.8*np.sqrt(rects[0][2]*rects[0][3]))
+        rects.append(tuple(list(rtag.astype(int))))
+    rect = rects[0]
+    d = TRASH.location3d_by_params(camera_data, 0.8*np.sqrt(rects[0][2]*rects[0][3]), (rect[0] + rect[2]/2, rect[1] + rect[3]/2))
 
-    return d, rects[0]
+    return d
