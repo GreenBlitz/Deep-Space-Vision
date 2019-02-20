716a0ccb7bc26215a58c038c66bdd1860f7bfcc0
matanraviv09 <matanraviv09@gmail.com>
2019-02-20 20:24:54 +0200

    fuck kedem

--- models/pipeline_consts.py
+++ models/pipeline_consts.py
@@ -35,33 +35,33 @@ find_contours = PipeLine(lambda frame: cv2.findContours(frame, cv2.RETR_TREE, cv
 
 sort_contours = PipeLine(lambda cnts: sorted(cnts, key=lambda x: cv2.contourArea(x), reverse=True))
 
-filter_contours = PipeLine((lambda cnts: filter(lambda c: cv2.contourArea(c) >= 6.0, cnts)))
+filter_contours = PipeLine((lambda cnts: filter(lambda c: cv2.contourArea(c) >= 6.0, cnts))) + list
 
 contour_center = PipeLine(lambda cnt: cv2.moments(cnt),
                           lambda m: (int(m['m10'] / (m['m00'] + 0.0000001)), int(m['m01'] / (m['m00'] + 0.0000001))))
 
-contours_centers = PipeLine(lambda cnts: map(contour_center, cnts))
+contours_centers = PipeLine(lambda cnts: map(contour_center, cnts)) + list
 
 # SHAPES
 
-contours_to_rects = PipeLine(lambda cnts: map(lambda x: cv2.boundingRect(x), cnts))
+contours_to_rects = PipeLine(lambda cnts: map(lambda x: cv2.boundingRect(x), cnts)) + list
 
-contours_to_rects_sorted = contours_to_rects + (lambda rects: sorted(rects, key=lambda x: x[2] * x[3], reverse=True))
+contours_to_rects_sorted = contours_to_rects + (lambda rects: sorted(rects, key=lambda x: x[2] * x[3], reverse=True)) + list
 
-contours_to_circles = PipeLine(lambda cnts: map(lambda x: cv2.minEnclosingCircle(x), cnts))
+contours_to_circles = PipeLine(lambda cnts: map(lambda x: cv2.minEnclosingCircle(x), cnts)) + list
 
-contours_to_circles_sorted = contours_to_circles + (lambda rects: sorted(rects, key=lambda x: x[1], reverse=True))
+contours_to_circles_sorted = contours_to_circles + (lambda rects: sorted(rects, key=lambda x: x[1], reverse=True)) + list
 
-contours_to_rotated_rects = PipeLine(lambda cnts: map(lambda x: cv2.minAreaRect(x), cnts))
+contours_to_rotated_rects = PipeLine(lambda cnts: map(lambda x: cv2.minAreaRect(x), cnts)) + list
 
 contours_to_rotated_rects_sorted = contours_to_rotated_rects + PipeLine(
-    lambda rects: sorted(rects, key=lambda x: x[1][0] * x[1][1]))
+    lambda rects: sorted(rects, key=lambda x: x[1][0] * x[1][1])) + list
 
 contours_to_polygons = PipeLine(lambda cnts: map(lambda cnt: (cnt, 0.05 * cv2.arcLength(cnt, True)), cnts),
                                 lambda cnts: map(lambda cnt0_eps1: cv2.approxPolyDP(cnt0_eps1[0], cnt0_eps1[1], True),
                                                  cnts),
                                 lambda polydps: map(lambda polydp: map(lambda x: x[0], polydp), polydps),
-                                lambda polydps: map(lambda polydp: list(map(tuple, polydp)), polydps))
+                                lambda polydps: map(lambda polydp: list(map(tuple, polydp)), polydps))  + list
 
 filter_inner_circles = PipeLine(funcs.filter_inner_circles)
 
--- test/network tests/stream test server.py
+++ test/network tests/stream test server.py
@@ -5,10 +5,13 @@ PORT = 8089
 
 
 def main():
-    server = StreamServer()
+    server1 = StreamServer(port=8089)
+    server2 = StreamServer(port=8090)
     while True:
-        frame = server.get_frame()
-        cv2.imshow('stream', frame)
+        frame1 = server1.get_frame()
+        frame2 = server2.get_frame()
+        cv2.imshow('stream1', frame1)
+        cv2.imshow('stream2', frame2)
         if cv2.waitKey(1) & 0xFF == ord('c'):
             cv2.destroyAllWindows()
             break
