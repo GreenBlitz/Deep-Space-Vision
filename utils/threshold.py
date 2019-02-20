aa3d8c0a177c948c9300b0459accae986e4a1bb1
matanraviv09 <matanraviv09@gmail.com>
2019-02-20 03:37:46 +0200

    colored a bunch of shit

--- utils/threshold.py
+++ utils/threshold.py
@@ -57,11 +57,71 @@ def bgr_threshold(frame, params):
     return cv2.inRange(frame, (red[0], green[0], blue[0]), (red[1], green[1], blue[1]))
 
 
+def luv_threshold(frame, params):
+    """
+    thresholds the image according to RGB values
+    :param frame: the image
+    :param params: the hls values, 3x2 matrix of [hmin hmax]
+                                                 [lmin lmax]
+                                                 [smin smax]
+    :return: binary threshold image
+    """
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2LUV)
+    red, green, blue = params
+    return cv2.inRange(frame, (red[0], green[0], blue[0]), (red[1], green[1], blue[1]))
+
+
+def lab_threshold(frame, params):
+    """
+    thresholds the image according to RGB values
+    :param frame: the image
+    :param params: the hls values, 3x2 matrix of [hmin hmax]
+                                                 [lmin lmax]
+                                                 [smin smax]
+    :return: binary threshold image
+    """
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2LAB)
+    red, green, blue = params
+    return cv2.inRange(frame, (red[0], green[0], blue[0]), (red[1], green[1], blue[1]))
+
+
+def yuv_threshold(frame, params):
+    """
+    thresholds the image according to RGB values
+    :param frame: the image
+    :param params: the hls values, 3x2 matrix of [hmin hmax]
+                                                 [lmin lmax]
+                                                 [smin smax]
+    :return: binary threshold image
+    """
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2YUV)
+    red, green, blue = params
+    return cv2.inRange(frame, (red[0], green[0], blue[0]), (red[1], green[1], blue[1]))
+
+
+def xyz_threshold(frame, params):
+    """
+    thresholds the image according to RGB values
+    :param frame: the image
+    :param params: the hls values, 3x2 matrix of [hmin hmax]
+                                                 [lmin lmax]
+                                                 [smin smax]
+    :return: binary threshold image
+    """
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2XYZ)
+    red, green, blue = params
+    return cv2.inRange(frame, (red[0], green[0], blue[0]), (red[1], green[1], blue[1]))
+
+
 THRESHOLD_NAME_TABLE = {
     'BGR': bgr_threshold,
     'RGB': rgb_threshold,
     'HLS': hls_threshold,
-    'HSV': hsv_threshold
+    'HSV': hsv_threshold,
+    'LUV': luv_threshold,
+    'LAB': lab_threshold,
+    'YUV': yuv_threshold,
+    'XYZ': xyz_threshold
 }
 
 
