bde8222d6f964f36606cd6e2a4fc98944323a8d9
GB Software laptop <greenblitz.programmers@gmail.com>
2019-03-04 13:48:31 +0200

    b

--- plan_b_reciever.py
+++ plan_b_reciever.py
@@ -13,6 +13,7 @@ def main():
             frame = pickle.loads(frame)
             frame = cv2.imdecode(frame, -1)
             # cv2.circle(frame, (frame.shape[1]//2, frame.shape[0]//2), 2, (0, 255, 0), 2)
+            frame = cv2.resize(frame, (0, 0), fx=3, fy=3)
             cv2.imshow('stream', frame)
             cv2.waitKey(1)
 
